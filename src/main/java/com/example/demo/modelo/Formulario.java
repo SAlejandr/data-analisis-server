4f1987cb7b81ad27a0d5e24778e412aafb607fc1
SAlejandr <alejitocastaneda1@gmail.com>
2022-04-26 16:53:28 +0200

    Base de formulario

--- src/main/java/com/example/demo/modelo/Formulario.java
+++ src/main/java/com/example/demo/modelo/Formulario.java
@@ -4,14 +4,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
+import javax.persistence.Id;
+import java.util.HashMap;
+
 @Data
 
 @Document
 public class Formulario {
 
-	
+	@Id
 	private String id;
-	
-	
+
+	private String nombre;
+
+	private HashMap<String, Tipo> campos;
 	
 }
